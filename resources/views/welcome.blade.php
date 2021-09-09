a4f850fb7c3379296776905f8737e4dc992ae6c0
Kyaw Zayya <kyawzayya.ucsp@gmail.com>
2021-09-09 23:24:32 +0630

    Finish

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,6 +8,29 @@
                     <img class="object-contain shadow-lg rounded-lg" src="{{ asset('book.jpg') }}" alt="">
                 </div>
             </div>
+            @if (session()->has('message'))
+            <div class="-ml-0 -mt-2 flex py-2 px-4 sm:px-2 lg:px-4">
+                <div class="-ml-4 -mt-2 flex">
+                    <div class="rounded-md bg-green-50 p-4">
+                        <div class="flex">
+                            <div class="flex-shrink-0">
+                                <svg class="h-5 w-5 text-green-400" x-description="Heroicon name: solid/check-circle"
+                                    xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                                    <path fill-rule="evenodd"
+                                        d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
+                                        clip-rule="evenodd"></path>
+                                </svg>
+                            </div>
+                            <div class="ml-3">
+                                <h3 class="text-sm font-medium text-green-800">
+                                    {{ session()->get('message') }}
+                                </h3>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            @endif
             <form class="space-y-8 divide-gray-200" action="{{ route('orders.store') }}" method="POST"
                 enctype="multipart/form-data">
                 @csrf
