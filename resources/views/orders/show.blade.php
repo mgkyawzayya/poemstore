2c3f6cab5e5b3eeff980be058a86262f8bec06f7
Kyaw Zayya <kyawzayya.ucsp@gmail.com>
2021-09-09 23:52:39 +0630

    Finish

--- resources/views/orders/show.blade.php
+++ resources/views/orders/show.blade.php
@@ -69,7 +69,7 @@
                                         class="inline-flex items-center px-6 py-3 border border-transparent text-base font-medium rounded-md shadow-sm text-white bg-red-600 hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 mr-4">
                                         Confirm
                                     </button>
-                                    <a href="{{ route('orders.index') }}">
+                                    <a href="{{ route('pending-orders.index') }}">
                                         <button type="button"
                                             class="inline-flex items-center px-6 py-3 border border-transparent text-base font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
                                             Back
